8f073b57d2c3a2e6ca5bca021ec0f1361cf7750b
Georgio Feghali <75293296+GeorgioFe@users.noreply.github.com>
2022-11-25 20:13:01 -0800

    Done with grade.sh and Test file.

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,10 +1,91 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class TestListExamples {
-  @Test(timeout = 100)
-  public void testTimeout() {
-    while(true) {}
+
+  @Test
+  public void testFilter1(){
+    StringChecker sc = new StringChecker(){
+      public boolean checkString(String s){
+        return true;
+      }
+    };
+    
+    List<String> input = new ArrayList<String>();
+    input.add("hello");
+    input.add("bye");
+    input.add("test");
+    input.add("computer");
+    List<String> result = ListExamples.filter(input, sc);
+    List<String> expect = new ArrayList<>();
+    expect.add("hello");
+    expect.add("bye");
+    expect.add("test");
+    expect.add("computer");
+
+    assertEquals(expect, result);
   }
-  // Write your grading tests here!
+
+  @Test
+  public void testFilter2(){
+    StringChecker sc = new StringChecker(){
+      public boolean checkString(String s){
+        return true;
+      }
+    };
+    
+    List<String> input = new ArrayList<String>();
+    List<String> result = ListExamples.filter(input, sc);
+    List<String> expect = new ArrayList<>();
+
+    assertEquals(expect, result);
+  }
+
+  @Test
+  public void testFilter3(){
+    StringChecker sc = new StringChecker(){
+      public boolean checkString(String s){
+        return true;
+      }
+    };
+    
+    List<String> input = new ArrayList<String>();
+    input.add("hello");
+    input.add("bye");
+    input.add("hello");
+    input.add("bye");
+    List<String> result = ListExamples.filter(input, sc);
+    List<String> expect = new ArrayList<>();
+    expect.add("hello");
+    expect.add("bye");
+    expect.add("hello");
+    expect.add("bye");
+
+    assertEquals(expect, result);
+  }
+
+  @Test
+  public void testMerge(){
+    List<String> list1 = new ArrayList<>();
+    List<String> list2 = new ArrayList<>();
+    list1.add("a");
+    list1.add("c");
+    list1.add("e");
+    list2.add("b");
+    list2.add("d");
+    list2.add("f");
+    List<String> expected = new ArrayList<>();
+    expected.add("a");
+    expected.add("b");
+    expected.add("c");
+    expected.add("d");
+    expected.add("e");
+    expected.add("f");
+    
+    assertEquals(expected, ListExamples.merge(list1, list2));
+  }
+
 }
--- grade.sh
+++ grade.sh
@@ -2,7 +2,41 @@
 
 rm -rf student-submission
 git clone $1 student-submission
-echo 'Finished cloning'
+
+# Check if repo cloned successfully.
+if test -d ./student-submission
+then
+    echo "  Cloning Finished."
+else
+    echo "  Could not Clone Repo."
+fi
+
+cp TestListExamples.java student-submission
+cp -r lib student-submission
+
+cd student-submission
+
+# Check if the file exists.
+if test -e ListExamples.java
+then
+    echo "  Contains file."
+else
+    echo "  ListExamples.java not found."
+    exit 1
+fi
+
+# Compile and Run
+if javac -cp .:./lib/hamcrest-core-1.3.jar:./lib/junit-4.13.2.jar *.java 2> compile-err.txt
+then
+    echo "  Compiled."
+    java -cp .:./lib/hamcrest-core-1.3.jar:./lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > test.txt
+    echo "  "
+    cat test.txt
+else
+    echo "  Compile Error."
+    echo "  "
+    cat compile-err.txt
+fi
 
 
 
